a21fc9de8c84752c664153f2ecc4e9e8a9bd07c6
TheSillyOk <priv.ld@proton.me>
2026-03-28 15:36:07 -0300

    improve: simplify customize.sh by not skipping unzip
    Signed-off-by: TheSillyOk <priv.ld@proton.me>

--- module/customize.sh
+++ module/customize.sh
@@ -1,5 +1,4 @@
 # shellcheck disable=SC2034
-SKIPUNZIP=1
 
 NAME=$(grep_prop name "${TMPDIR}/module.prop")
 ID=$(grep_prop id "${TMPDIR}/module.prop")
@@ -34,67 +33,37 @@ if [[ "$CPU_ABIS" == *"x86"* && "$CPU_ABIS" != "x86_64" || "$CPU_ABIS" == *"arme
   ui_print "- Device supports 32-bit"
 fi
 
-abort_verify() {
-  ui_print "***********************************************************"
-  ui_print "! $1"
-  ui_print "! This zip is incomplete"
-  abort    "***********************************************************"
-}
-
-extract() {
-  local zip="$1"
-  local target="$2"
-  local dir="$3"
-  local junk_paths="${4:-false}"
-  local opts="-o"
-  local target_path
-
-  [[ "$junk_paths" == true ]] && opts="-oj"
-
-  if [[ "$target" == */ ]]; then
-    target_path="$dir/$(basename "$target")"
-    unzip $opts "$zip" "${target}*" -d "$dir" >&2
-    [[ -d "$target_path" ]] || abort_verify "$target directory doesn't exist"
+if [ "$ARCH" = "x86" ] || [ "$ARCH" = "x64" ]; then
+  rm -r "$MODPATH/zygisk/arm"*
+  if [[ "$SUPPORTS_64BIT" == "true" ]]; then
+    ui_print "- Keeping x64 libraries"
   else
-    target_path="$dir/$(basename "$file")"
-    unzip $opts "$zip" "$target" -d "$dir" >&2
-    [[ -f "$target_path" || -d "$target_path" ]] || abort_verify "$target file doesn't exist"
+    rm "$MODPATH/zygisk/x86_64.so"
   fi
-}
 
-ui_print "- Extracting module files"
-extract "$ZIPFILE" 'module.prop'     "$MODPATH"
-if [ -f "/data/adb/modules/${ID}/config.json" ]; then
-  ui_print "- Found existing config"
-  cp "/data/adb/modules/${ID}/config.json" "$MODPATH"
+  if [[ "$SUPPORTS_32BIT" == "true" ]]; then
+    ui_print "- Keeping x86 libraries"
+  else
+    rm "$MODPATH/zygisk/x86.so"
+  fi
 else
-  extract "$ZIPFILE" 'config.json'     "$MODPATH"
+  rm -r "$MODPATH/zygisk/x86"*
+  if [[ "$SUPPORTS_64BIT" == "true" ]]; then
+    ui_print "- Keeping arm64 libraries"
+  else
+    rm "$MODPATH/zygisk/arm64-v8a.so"
+  fi
+
+  if [[ "$SUPPORTS_32BIT" == "true" ]]; then
+    ui_print "- Keeping arm libraries"
+  else
+    rm "$MODPATH/zygisk/armeabi-v7a.so"
+  fi
 fi
 
-if [ "$ARCH" = "x86" ] || [ "$ARCH" = "x64" ]; then
-  if [[ "$SUPPORTS_64BIT" == "true" ]]; then
-    ui_print "- Extracting x64 libraries"
-    extract "$ZIPFILE" 'zygisk/x64.so' "$MODPATH/zygisk" true
-    mv "$MODPATH/zygisk/x64.so" "$MODPATH/zygisk/x86_64.so"
-  fi
-
-  if [[ "$SUPPORTS_32BIT" == "true" ]]; then
-    ui_print "- Extracting x86 libraries"
-    extract "$ZIPFILE" 'zygisk/x86.so' "$MODPATH/zygisk" true
-    mv "$MODPATH/zygisk/x86.so" "$MODPATH/zygisk/x86.so"
-  fi
-else
-  if [[ "$SUPPORTS_64BIT" == "true" ]]; then
-    ui_print "- Extracting arm64 libraries"
-    extract "$ZIPFILE" 'zygisk/arm64-v8a.so' "$MODPATH/zygisk" true
-    mv "$MODPATH/zygisk/arm64-v8a.so" "$MODPATH/zygisk/arm64-v8a.so"
-  fi
-
-  if [[ "$SUPPORTS_32BIT" == "true" ]]; then
-    ui_print "- Extracting arm libraries"
-    extract "$ZIPFILE" 'zygisk/armeabi-v7a.so' "$MODPATH/zygisk" true
-    mv "$MODPATH/zygisk/armeabi-v7a.so" "$MODPATH/zygisk/armeabi-v7a.so"
-  fi
+if [ -f "/data/adb/modules/${ID}/config.json" ]; then
+  ui_print "- Keeping existing config"
+  cp "/data/adb/modules/${ID}/config.json" "$MODPATH"
 fi
 
 ui_print "- Welcome to $NAME $VERSION"
